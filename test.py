8c2d4aaa04e9bdb26b4db44de11c9b71ae5e9a5b
Saly <saly@juke4me.com>
2018-10-29 23:38:49 +0530

    Complete the code and add a picture

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 # module and library required to build a Face Recognition System
 import face_recognition
+import numpy as np
 import cv2
 # objective: this code will help you in running face recognition on a video file and saving the results to a new video file.
 # Open the input movie file
 # "VideoCapture" is a class for video capturing from video files, image sequences or cameras
-input_video = cv2.VideoCapture("input.mp4")
+input_video = cv2.VideoCapture("vadachennaiteaser.mp4")
 #"CAP_PROP_FRAME_COUNT": it helps in finding number of frames in the video file.
 length = int(input_video.get(cv2.CAP_PROP_FRAME_COUNT))
 # Create an output movie file (make sure resolution/frame rate matches input video!)
@@ -13,14 +14,60 @@ length = int(input_video.get(cv2.CAP_PROP_FRAME_COUNT))
 fourcc = cv2.VideoWriter_fourcc('M','P','E','G')
 # 25.07-  number of frames per second (fps)
 #(1280,720)- frame size
-output_video = cv2.VideoWriter('output.avi', fourcc, 25.07, (1280, 720))
+output_video = cv2.VideoWriter('output2.avi', fourcc, 25.07, (1280, 720))
 # Load some sample pictures and learn how to recognize them.
-#female_image = face_recognition.load_image_file("warina.jpg")
-#female_face_encoding = face_recognition.face_encodings(female_image)[0]
+female_image = face_recognition.load_image_file("Andrea.jpg")
+female_face_encoding = face_recognition.face_encodings(female_image)[0]
 #  "face_recognition.face_encodings": it's a face_recognition package which returns a list of 128-dimensional face encodings
-male_image = face_recognition.load_image_file("Dhanush.jpeg")
+male_image = face_recognition.load_image_file("Dhanush.jpg")
 male_face_encoding = face_recognition.face_encodings(male_image)[0]
 
 known_faces = [
+    female_face_encoding,
     male_face_encoding
 ]
+
+face_locations = []
+face_encodings = []
+face_names = []
+frame_number = 0
+while True:
+    # Grab a single frame of video
+    ret, frame = input_video.read()
+    frame_number += 1
+# Quit when the input video file ends
+    if not ret:
+        break
+# Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_frame = frame[:, :, ::-1]
+# Find all the faces and face encodings in the current frame of video
+    face_locations = face_recognition.face_locations(rgb_frame)
+    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+    face_names = []
+    for face_encoding in face_encodings:
+        # See if the face is a match for the known face(s)
+        match = face_recognition.compare_faces(known_faces, face_encoding, tolerance=0.50)
+
+        name = None
+        if match[0]:
+            name = "Andrea"
+        elif match[1]:
+            name = "Dhanush"
+
+        face_names.append(name)
+    # Label the results
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        if not name:
+            continue
+# Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+# Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 25), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.5, (255, 255, 255), 1)
+# Write the resulting image to the output video file
+    print("Writing frame {} / {}".format(frame_number, length))
+    output_video.write(frame)
+# All done!
+input_video.release()
+cv2.destroyAllWindows()
